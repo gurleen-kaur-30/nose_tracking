0ac27f8ca88a3c58055efbe6c705bd849c1d135f
gurleen-kaur-30 <gurleen.kaur@iiitb.org>
2020-10-05 21:00:37 +0530

    tracker.update giving false

--- face_detection.py
+++ face_detection.py
@@ -1,14 +1,11 @@
 import numpy as np
 import cv2
 
-def getNose(x,y,w,h,img):
-    centre_coordinates = (x+w/2, y+h/2)
-    cv2.circle(img, centre_coordinates, 30, (255,0,0), 2)
-
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
+cnt = 10
 while True:
     ret, img = cap.read()
     img = cv2.flip(img, 4)
@@ -19,12 +16,33 @@ while True:
         minNeighbors=5,     
         minSize=(30, 30)
     )
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]  
-        getNose(x,y,w,h,img)
-    cv2.imshow('video',img)
+    tracker = cv2.TrackerCSRT_create()
+    if(cnt):
+        print("hey")
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]  
+            bbox = x+w/2 -20, y+h/2 - 20, 40, 40
+    	    cv2.rectangle(img, bbox, (0,255,0), 2)
+            tracker.init(img, bbox)
+            cv2.imshow('video',img)
+	    cnt = cnt - 1
+    else:
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            ok, bbox = tracker.update(img)
+            print(bbox)
+            if ok:
+                print("hello")
+                (x, y, w, h) = [int(v) for v in bbox]
+                cv2.rectangle(img, (x, y), (x + w, y + h),(0, 255, 0), 2)
+            cv2.imshow('video',img)
+
+            
+        
     k = cv2.waitKey(30) & 0xff
     if k == 27: # press 'ESC' to quit
         break
