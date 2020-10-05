981ce2ec37dcd0ec101e085b2993ad80c6e562e2
timetraveler05 <anubhavmishra.chimpu@gmail.com>
2020-10-05 23:54:12 +0530

    working kinda

--- new.py
+++ new.py
@@ -3,6 +3,7 @@ import imutils
 import time
 from imutils.video import VideoStream
 from imutils.video import FPS
+import pyautogui
 
 tracker = cv2.TrackerCSRT_create()
 
@@ -25,11 +26,29 @@ while True:
 	# grab the new bounding box coordinates of the object
         (success, box) = tracker.update(frame)
 
-		# check to see if the tracking was a success
+	# check to see if the tracking was a success
         if success:
             (x, y, w, h) = [int(v) for v in box]
             cv2.rectangle(frame, (x, y), (x + w, y + h),
 				(0, 255, 0), 2)
+            center = (int(x + w/2), int(y + h/2))
+            cv2.circle(frame, (center[0], center[1]), 10, (0, 255, 0), 1)
+            if center[0] < W/3:
+                print("left")
+                pyautogui.keyDown('left')
+                pyautogui.keyUp('left')
+            elif center[0] > 2*W/3:
+                print("right")
+                pyautogui.keyDown('right')
+                pyautogui.keyUp('right')
+            if center[1] < H/3:
+                print("up")
+                pyautogui.keyDown('up')
+                pyautogui.keyUp('up')
+            elif center[1] > 2 * H/3:
+                print("down")
+                pyautogui.keyDown('down')
+                pyautogui.keyUp('down')
 
 		# update the FPS counter
         fps.update()
